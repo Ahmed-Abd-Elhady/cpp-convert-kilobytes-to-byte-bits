3e41509c05581919770cc7f6f8b1ef27c3ee1a70
Ahmed Abd-Elhady|コナファ <111198618+Ahmed-Abd-Elhady@users.noreply.github.com>
2023-11-12 18:46:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int user_input;
-    cout << "How mutch kilobytes to byte&bits do you want to convert ? : \n";
-    cin >> user_input;
-    cout << "The Number Of byte&bits bytes you write is :  " << user_input << "\n";
-    cout << "Convert From KiloBytes to byte = " << user_input * 1024 << " bytes \n";
-    cout << "Convert From KiloBytes to bits = " << user_input * 1024 * 8 << " bits \n";
+int user_age;
+int user_choose;
 
-    return 0;
+void showcase()
+{
+  cout << "-------/--------------/----\n";
+  cout << "-kilobytes to byte&bits Applection-\n";
+  cout << "---------------/-----------\n";
+  cout << "1-/=/=/=/Convert : \n";
+  cout << "2-/=/=/=/Exit : \n";
+  cin >> user_choose;
 }
 
-// Needed Output
-//"Hello Elzero Web School I Love Programming Too Much Specially C++"
+void cal()
+{
+
+  do
+  {
+    showcase();
+    switch (user_choose)
+    {
+    case 1:
+      int user_input;
+      cout << "How mutch kilobytes to byte&bits do you want to convert ? : \n";
+      cin >> user_input;
+      cout << "The Number Of byte&bits bytes you write is :  " << user_input << "\n";
+      cout << "Convert From KiloBytes to byte = " << user_input * 1024 << " bytes \n";
+      cout << "Convert From KiloBytes to bits = " << user_input * 1024 * 8 << " bits \n";
+      cal();
+      break;
+    }
+  } while (user_choose < 1);
+}
+
+int main()
+{
+  cal();
+}
